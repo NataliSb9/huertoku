897cd6dc8ecc8600698e1f3abedc3785e8562891
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-12 19:17:21 +0200

    use herokuapp name

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/huertoku'));
+app.use(express.static(__dirname + '/dist/huertaweb'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/huertoku/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/huertaweb/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
