2395b62f89a8ee447248b042728943b98c3523c0
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-12 17:39:11 +0200

    cambio ruta en server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/huertoku'));
+app.use(express.static(__dirname + '/dist/ProyectoFinalLaHuertaAngular'));
 app.get('/*', function(req,res){
-    res.sendFile("index.html", {root: __dirname + '/dist/huertoku'});
+    res.sendFile("index.html", {root: __dirname + '/dist/ProyectoFinalLaHuertaAngular'});
 });
 
 app.listen(process.env.PORT || 8080);
