442573f8df3279a01aeea6cc428b02dcfbe295d9
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-12 19:24:07 +0200

    use node project name

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/huertaweb'));
+app.use(express.static(__dirname + '/dist/la-huerta'));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/huertaweb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/la-huerta/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
