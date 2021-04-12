37c3405b88313830bc64ead73e678ea15e511f8b
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-12 19:00:31 +0200

    test index.html new path

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/huertoku'));
+
 app.get('/*', function(req,res){
-    res.sendFile("index.html", {root: __dirname + '/dist/huertoku'});
+    res.sendFile(path.join(__dirname + '/dist/huertoku/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
