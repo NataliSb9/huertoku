61106537a0f1106fff6d1a5e43121703da2222ab
pascual <pjbolullo@gmail.com>
2021-04-19 13:09:56 +0200

    editar perfil completado y corregido

--- src/app/pages/perfil-editar/perfil-editar.component.ts
+++ src/app/pages/perfil-editar/perfil-editar.component.ts
@@ -10,111 +10,132 @@ import { UserService } from 'src/app/shared/user.service';
 export class PerfilEditarComponent implements OnInit {
   public usuarioLogeado:User
   public message;
+  public edit;
+  public error;
   constructor(private userService: UserService) { 
     this.usuarioLogeado=this.userService.user
+    this.edit = false
+    this.error = false
   }
 
-  saveChanges(email:string, password:string, name:string, surname1:string, birthYear:number, username:string, localidad:string, tel:number, userImg:string){
-    console.log(this.usuarioLogeado)
-    this.userService.editProfile(new User(email,password,this.usuarioLogeado.iduser,name,surname1,birthYear,username,localidad,tel,userImg)).subscribe((data:any)=>{
+  // saveChanges(email:string, password:string, name:string, surname1:string, birthYear:number, username:string, localidad:string, tel:number, userImg:string){
+  //   console.log(this.usuarioLogeado)
+  //   this.userService.editProfile(new User(email, password, this.usuarioLogeado.iduser, name, surname1, birthYear, username, localidad, tel, userImg)).subscribe((data:any)=>{
         
-        this.usuarioLogeado=data
-        this.message="Dato/s modificados"
-        console.log(data);
-        
-      // this.usuarioLogeado=data
-    })
-  }
-  // editarDisco( name:string, surname1:string, birthYear:number, username:string, localidad:string, tel:number, email:string ,userImg:string) 
-  // {
-  //   if (iduser !== undefined) 
-  // {
-  //     if (name === "") 
-  //     {
-  //       name = null
-  //     } 
-  //     else 
-  //     {
-  //       name = name
-  //     }
-
-  //     if (surname1 === "") 
-  //     {
-  //       surname1 = null
-  //     } 
-  //     else 
-  //     {
-  //       surname1 = surname1
-  //     }
-
-  //     if (birthYear === "") 
-  //     {
-  //       birthYear = null
-  //     } 
-  //     else 
-  //     {
-  //       birthYear = birthYear
-  //     }   
-
-  //     if (username === "") 
-  //     {
-  //       username = null
-  //     } 
-  //     else 
-  //     {
-  //       username = username
-  //     }
-
-  //     if (localidad === undefined) 
-  //     {
-  //       localidad = null
-  //     } 
-  //     else 
-  //     {
-  //       localidad = localidad
-  //     } 
-
-  //     if (tel === null) 
-  //     {
-  //       tel = null
-  //     } 
-  //     else 
-  //     {
-  //       tel = tel
-  //     } 
-
-  //     if (email === undefined) 
-  //     {
-  //       email = null
-  //     } 
-  //     else 
-  //     {
-  //       email = email
-  //     }
-
-  //     if (userImg === undefined) 
-  //     {
-  //       userImg = null
-  //     } 
-  //     else 
-  //     {
-  //       userImg = userImg
-  //     }
-  //     let discoNew = new User()
-
-  //     console.log(discoNew)
-  //     this.userService.editProfile(discoNew).subscribe((respuesta: User) => {
-  //       console.log(respuesta)
-  //       this.edit= true;
-  //     },(error) =>{
-  //       console.log(error.error)
-  //       this.error = error.error
-  //     })
-  //   } else {
-  //     console.log("No se puede editar")
-  //     this.edit = false
-  //   }
+  //       this.usuarioLogeado=data
+  //       this.message="Dato/s modificados"
+  //       console.log(data);
+      
+  //   })
   // }
+  ////////////////////////////////////////////////
+  saveChanges(email:string, password:string, name:string, surname1:string, birthYear:number, username:string, localidad:string, tel:number, userImg:string) 
+  {
+    if (this.usuarioLogeado.iduser !== undefined) 
+  {
+      if (email === "") 
+      {
+        email = null
+      } 
+      else 
+      {
+        email = email
+      }
+
+      if (password === "") 
+      {
+        password = null
+      } 
+      else 
+      {
+        password = password
+      }
+
+      if (name === "") 
+      {
+        name = null
+      } 
+      else 
+      {
+        name = name
+      }
+
+      if (surname1 === "") 
+      {
+        surname1 = null
+      } 
+      else 
+      {
+        surname1 = surname1
+      }
+
+      if (birthYear === null) 
+      {
+        birthYear = null
+      } 
+      else 
+      {
+        birthYear = birthYear
+      }   
+
+      if (username === "") 
+      {
+        username = null
+      } 
+      else 
+      {
+        username = username
+      }
+
+      if (localidad === "") 
+      {
+        localidad = null
+      } 
+      else 
+      {
+        localidad = localidad
+      } 
+
+      if (tel === null) 
+      {
+        tel = null
+      } 
+      else 
+      {
+        tel = tel
+      } 
+
+      if (userImg === "") 
+      {
+        userImg = null
+      } 
+      else 
+      {
+        userImg = userImg
+      }
+
+      let usuarioModificado = new User(email, password, this.usuarioLogeado.iduser, name, surname1, birthYear, username, localidad, tel, userImg)
+
+      console.log(usuarioModificado)
+      this.userService.editProfile(usuarioModificado).subscribe((respuesta: User) => {
+
+        console.log(respuesta)
+        this.message="Dato/s modificados"
+        this.edit= true;
+        respuesta = this.usuarioLogeado
+
+      },
+      (error) =>{
+
+        console.log(error)
+
+      })
+  }  else 
+      {
+        console.log("No se puede editar")
+        this.edit = false
+      }
+  }
 
   ngOnInit(): void {
   }
