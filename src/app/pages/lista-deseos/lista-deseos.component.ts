846afd5688983c142ceef863cb1eeb189047982e
jhurtado96 <javierhurtado98@gmail.com>
2021-04-20 10:42:36 +0200

    crear objeto chat

--- src/app/pages/lista-deseos/lista-deseos.component.ts
+++ src/app/pages/lista-deseos/lista-deseos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chat } from 'src/app/model/chat';
 import { Product } from 'src/app/model/product';
+import { ChatService } from 'src/app/shared/chat.service';
 import { ProductosService } from 'src/app/shared/productos.service';
 import { UserService } from 'src/app/shared/user.service';
 
@@ -11,9 +13,20 @@ import { UserService } from 'src/app/shared/user.service';
 export class ListaDeseosComponent implements OnInit {
   public productos:Product[]
   public productito:Product
-  constructor(public productService:ProductosService, public userService:UserService) { 
+  public chat:Chat
+  constructor(public productService:ProductosService, public userService:UserService, public chatService:ChatService) { 
     this.productos=this.productService.productos
     
+  }
+
+  abrirChat(id:number){
+
+    console.log(id)
+    console.log(this.userService.user.iduser)
+    this.chatService.crearChat(new Chat(this.userService.user.iduser,id,0)).subscribe((data:Chat)=>{
+      this.chatService.chat=data
+      console.log(data)
+    })
   }
 
   ngOnInit(): void {
