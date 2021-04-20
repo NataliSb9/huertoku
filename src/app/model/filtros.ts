d4c8ef382b29a6f4d2246a803575e36dd4ad0262
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-20 11:33:38 +0200

    Filtros agregados

---
+++ src/app/model/filtros.ts
@@ -0,0 +1,8 @@
+export class Filtros {
+    nombreFiltro: string;
+    valorFiltro: string
+    constructor(nombreFiltro: string, valorFiltro: string){
+        this.nombreFiltro = nombreFiltro;
+        this.valorFiltro = valorFiltro;
+    }
+}
