adcca7af74e1a90433cd7d5bc2be716e379171f3
NataliSb9 <nsainzbarroso@gmail.com>
2021-04-21 17:29:49 +0200

    cambios en filtros + funcionalidad precio+ funcionalidad busqueda similar

--- src/app/model/filtros.ts
+++ src/app/model/filtros.ts
@@ -1,7 +1,7 @@
 export class Filtros {
     nombreFiltro: string;
     valorFiltro: string
-    constructor(nombreFiltro: string, valorFiltro: string) {
+    constructor(nombreFiltro: string, valorFiltro: any) {
         this.nombreFiltro = nombreFiltro;
         this.valorFiltro = valorFiltro;
     }
